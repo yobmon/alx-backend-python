5230de7b188b0b3741a4c50f6c0ae45bda32926b
yobmon <eyobsolomon33@gmail.com>
2025-11-14 09:39:33 +0300

    intial django project

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from utils import access_nested_map
 from parameterized import parameterized
 from utils import get_json
 from unittest.mock import patch,Mock
+from utils import memoize
 class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand( [
@@ -23,7 +24,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-class   TestGetJson(unittest.TestCase):
+class TestGetJson(unittest.TestCase):
     @patch("utils.requests.get")
     def test_get_json(self, mock_get):
         mock_response = Mock()
@@ -35,7 +36,22 @@ class   TestGetJson(unittest.TestCase):
         result = get_json(url)
         self.assertEqual(result, expected_payload)
         mock_get.assert_called_once_with(url)
+class TestMemoize(unittest.TestCase):
+    class test_memoize:
+         def a_method(self):
+          return 42
 
+         @memoize
+         def a_property(self):
+          return self.a_method()
+    def test_memoize(self):
+        test_instance = self.test_memoize()
+        with patch.object(test_instance, 'a_method', return_value=42) as mock_a_method:
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_a_method.assert_called_once()
         
 if __name__ == '__main__':
     unittest.main()

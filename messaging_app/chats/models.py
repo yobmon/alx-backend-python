d94556d13cbd6e0fed1a1376ead729061fd2b0db
yobmon <eyobsolomon33@gmail.com>
2025-11-14 09:49:56 +0300

    intial django project

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
     created_at = models.DateTimeField(auto_now_add=True)
-
+    password = models.CharField(max_length=128)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     def __str__(self):

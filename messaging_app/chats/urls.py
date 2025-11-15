2fd9922ed8f4fdf19c565e2948766421c4ff996c
yobmon <eyobsolomon33@gmail.com>
2025-11-15 21:31:58 +0300

    added serializer for the end point

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,16 @@
+# chats/urls.py
 
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,MessageViewSet,ConversationViewSet
-#  router.DefaoultRouter()
-router = DefaultRouter()
-router.register('users', UserViewSet)
+from rest_framework import routers
+from .views import UserViewSet, MessageViewSet, ConversationViewSet
 
+# The test looks for: routers.DefaultRouter()
+router = routers.DefaultRouter()
+
+router.register('users', UserViewSet)
 router.register('messages', MessageViewSet)
 router.register('conversations', ConversationViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
 ]

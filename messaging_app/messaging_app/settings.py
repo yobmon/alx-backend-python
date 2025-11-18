64f7a427af3243289dbf95268f0713f501ecedbb
yobmon <eyobsolomon33@gmail.com>
2025-11-18 16:44:44 +0300

    added drf configuration

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -130,11 +130,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
-     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 AUTH_USER_MODEL = 'chats.User'
